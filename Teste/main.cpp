1d31f60f5a3a961113125e9f88f9118299e31998
pheduarte <pheambrosio@gmail.com>
2024-11-08 15:45:18 +1100

    Initial Commit

---
+++ Teste/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Teste
+//
+//  Created by Phelippe D Ambrosio on 8/11/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
